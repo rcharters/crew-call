bde6eb7ed07f455c4f6a25dddc00cf38d03eaaac
Robin Charters <admin@robincharters.com>
2013-05-14 14:38:23 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrewCall::Application.config.secret_token = '2fa1221f4fd0a7fd9b17c211b91f0869ef951654a416ed69ca1dcd8e11c52ed62b29d1ffded59ddb05253bc9de93af44c3565e48510c2cccd87710fa40441147'
